1e34e063cfed7014a8a987ea94836bcd8df81d4e
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-27 10:09:54 +0100

    Create index, show, create action and views/show for Players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,30 @@
 class PlayersController < ApplicationController
+
   def index
+  	@players = Player.all.order("created_at DESC")
   end
+
+  def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+	  if @player.save
+	    redirect_to @player
+	  else
+	    render 'new'
+	  end
+  end
+
+  def show
+
+  end
+
+private
+
+	def player_params
+		params.require(:player).permit(:name, :surname, :age, :nationality, :position, :club)
+	end
+
 end
