519b46be9f540e7f648b4fbf084aea22baba2e97
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-30 16:45:47 +0100

    Add validates for model Player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,10 @@
 class Player < ApplicationRecord
+  
+  validates :name, presence: true, length: {minimum: 2}
+  validates :surname, presence: true, length: {minimum: 2}
+  validates :age, presence: true
+  validates :nationality, presence: true
+  validates :position, presence: true
+  validates :club, presence: true
+  
 end
