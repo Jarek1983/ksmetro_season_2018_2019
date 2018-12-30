17bc81b9585f3f5ec1ae6424b34189b0a1727fda
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-30 16:55:36 +0100

    Change seed for Players

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,114 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Player.new(name: "Bartosz", 
+	       surname: "Mariański", 
+	       age: 26, 
+	       nationality: "Polska", 
+	       position: "Libero", 
+	       club: "GKS Katowice").save
+
+Player.new(name: "Karol", 
+	       surname: "Butryn", 
+	       age: 25, 
+	       nationality: "Polska", 
+	       position: "Atakujący", 
+	       club: "GKS Katowice").save
+
+Player.new(name: "Alek", 
+	       surname: "Achrem", 
+	       age: 35, 
+	       nationality: "Białoruś", 
+	       position: "Przyjmujący", 
+	       club: "Aluron Virtu Warta Zawiercie").save
+
+Player.new(name: "Patryk", 
+	       surname: "Akala", 
+	       age: 30, 
+	       nationality: "Polska", 
+	       position: "Środkowy", 
+	       club: "Chemik Bydgoszcz").save
+
+Player.new(name: "Robbert", 
+	       surname: "Andriga", 
+	       age: 28, 
+	       nationality: "Holandia", 
+	       position: "Przyjmujący", 
+	       club: "Indykpol AZS Olsztyn").save
+
+Player.new(name: "Krzysztof", 
+	       surname: "Andrzejewski", 
+	       age: 35, 
+	       nationality: "Polska", 
+	       position: "Libero", 
+	       club: "Aluron Virtu Warta Zawiercie").save
+
+Player.new(name: "Rafael Araujo", 
+	       surname: "Araujo", 
+	       age: 27, 
+	       nationality: "Brazylia", 
+	       position: "Atakujący", 
+	       club: "ONICO Warszawa").save
+
+Player.new(name: "Filip", 
+	       surname: "Biegun", 
+	       age: 22, 
+	       nationality: "Polska", 
+	       position: "Przyjmujący", 
+	       club: "Cuprum Lubin").save
+
+Player.new(name: "Mateusz ", 
+	       surname: "Bieniek", 
+	       age: 24, 
+	       nationality: "Polska", 
+	       position: "Środkowy", 
+	       club: "ZAKSA Kędzierzyn-Koźle").save
+
+Player.new(name: "Kacper", 
+	       surname: "Borkowski", 
+	       age: 21, 
+	       nationality: "Polska", 
+	       position: "Libero", 
+	       club: "chemik Bydgoszcz").save
+
+Player.new(name: "Grzegorz", 
+	       surname: "Bociek", 
+	       age: 27, 
+	       nationality: "Polska", 
+	       position: "Atakujący", 
+	       club: "Aluron Virtu Warta Zawiercie").save
+
+Player.new(name: "Damian", 
+	       surname: "Boruch", 
+	       age: 29, 
+	       nationality: "Polska", 
+	       position: "Środkowy", 
+	       club: "Cuprum Lubin").save
+
+Player.new(name: "Antoine", 
+	       surname: "Brizard", 
+	       age: 24, 
+	       nationality: "Francja", 
+	       position: "Rozgrywający", 
+	       club: "ONICO Warszawa").save
+
+Player.new(name: "Adrian", 
+	       surname: "Buchowski", 
+	       age: 27, 
+	       nationality: "Polska", 
+	       position: "Przyjmujący", 
+	       club: "Indykpol AZS Olsztyn").save
+
+Player.new(name: "Jakub", 
+	       surname: "Bucki", 
+	       age: 30, 
+	       nationality: "Polska", 
+	       position: "Atakujący", 
+	       club: "Jastrzębski Węgiel").save
+
+Player.new(name: "Rafał", 
+	       surname: "Buszek", 
+	       age: 31, 
+	       nationality: "Polska", 
+	       position: "Przyjmujący", 
+	       club: "ZAKSA Kędzierzyn-Koźle").save
