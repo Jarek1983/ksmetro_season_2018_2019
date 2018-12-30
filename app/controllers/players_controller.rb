159e0a1cb9b8e6e91eb97bb9b2b272aac9b125b0
Jaroslaw Zmijewski <jzmija@wp.pl>
2018-12-30 14:04:00 +0100

    Create/change action: index, show, new, create, edit, update, destroy, find_player, player_params for Players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,5 @@
 class PlayersController < ApplicationController
+  before_action :find_player, only: [:show, :edit, :update, :destroy]
 
   def index
   	@players = Player.all.order("created_at DESC")
@@ -9,19 +10,43 @@ class PlayersController < ApplicationController
   end
 
   def create
+
     @player = Player.new(player_params)
+
 	  if @player.save
-	    redirect_to @player
+	    redirect_to player_path(@player)
 	  else
 	    render 'new'
 	  end
+
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @movie.update(movie_params)
+        redirect_to movie_path(@movie)
+    else
+        render 'edit'
+    end
 
   end
 
+  def destroy
+      @player.destroy
+      redirect_to players_path
+  end
+
 private
+
+  def find_movie
+    @player = Player.find(params[:id])
+  end
 
 	def player_params
 		params.require(:player).permit(:name, :surname, :age, :nationality, :position, :club)
