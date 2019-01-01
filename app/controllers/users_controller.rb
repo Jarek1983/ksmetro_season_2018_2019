a302bda40ddf9493f06965ba8bc4f45839619fd8
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-01-01 14:00:59 +0100

    Add views, model, controller for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+
+	def index
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to root_path, notice: 
+			# zmienić ściężkę na np. players_path
+			"Welcome #{@user.login}! You successfully created an account." 
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:login, :email, :password, :password_confirmation)
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+	
   has_secure_password
+  validates :email, presence: true, uniqueness: true, 
+  :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :login, presence: true, uniqueness: true
+
 end
