92439bc6ff94a59d636f48d404ca4ef434488c09
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-01-01 16:05:08 +0100

    Add UserId for Player

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_01_114352) do
+ActiveRecord::Schema.define(version: 2019_01_01_150352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2019_01_01_114352) do
     t.string "photo_content_type"
     t.bigint "photo_file_size"
     t.datetime "photo_updated_at"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_players_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2019_01_01_114352) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "players", "users"
 end
