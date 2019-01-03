09441d6b8cdb6696099bb86165f83ef8b574cf8f
Jaroslaw Zmijewski <jzmija@wp.pl>
2019-01-03 16:31:46 +0100

    Add current_user/admin for views/controllers

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -50,7 +50,7 @@ private
   end
 
 	def player_params
-		params.require(:player).permit(:name, :surname, :age, :nationality, :position, :club, :photo)
+		params.require(:player).permit(:name, :surname, :age, :nationality, :position, :club, :photo, :user_id)
 	end
 
 end
